2d7ceacb7e14e1f20c7966ac7d5356c374ec0798
nemanjan00 <nemanjan00@gmail.com>
2018-08-31 11:17:07 +0200

    Simple http server

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Println("Hello, 世界")
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 
