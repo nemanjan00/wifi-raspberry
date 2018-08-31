2e8b02c0e5284a4895fca35165b6f6677c69ed05
nemanjan00 <nemanjan00@gmail.com>
2018-08-31 13:45:41 +0200

    Reorganize

--- backend/main.go
+++ backend/main.go
@@ -1,35 +1,11 @@
 package main;
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"strconv"
-	"bytes"
-
-	"./config"
+	"./web"
 );
 
-func getListenerParam() (string){
-	port := strconv.Itoa(config.GetPort());
-	hostname := config.GetHostname();
-
-	var param bytes.Buffer;
-	param.WriteString(hostname);
-	param.WriteString(":");
-	param.WriteString(port);
-
-	return param.String();
-}
-
 func main() {
-	fs := http.FileServer(http.Dir("./frontend/static"))
-	http.Handle("/", fs);
-
-	param := getListenerParam();
-
-	fmt.Printf("Starting wifi-raspberry on port %s.\n", param);
-	log.Fatal(http.ListenAndServe(param, nil));
+	web.ConfigureServer();
+	web.Start();
 }
 
---
+++ backend/web/web.go
@@ -0,0 +1,37 @@
+package web;
+
+import (
+	"../config"
+
+	"fmt"
+	"log"
+	"strconv"
+	"bytes"
+
+	"net/http"
+);
+
+func getListenerParam() (string){
+	port := strconv.Itoa(config.GetPort());
+	hostname := config.GetHostname();
+
+	var param bytes.Buffer;
+	param.WriteString(hostname);
+	param.WriteString(":");
+	param.WriteString(port);
+
+	return param.String();
+}
+
+func Start(){
+	param := getListenerParam();
+
+	fmt.Printf("Starting wifi-raspberry on port %s.\n", param);
+	log.Fatal(http.ListenAndServe(param, nil));
+}
+
+func ConfigureServer(){
+	fs := http.FileServer(http.Dir("./frontend/static"))
+	http.Handle("/", fs);
+}
+
